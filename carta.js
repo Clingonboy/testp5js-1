751064fc44d70e29574c6ab31568c592fc97bd3e
Eugenio <clingonboy@gmail.com>
2019-05-06 23:16:15 +0200

    completato il drag&drop

--- carta.js
+++ carta.js
@@ -1,23 +1,28 @@
 class Carta {
-    constructor(seme, valore, imgCarta){
-        this.seme = seme
-        this.valore = valore
-        this.imgCarta = imgCarta
-        this.x = 0
-        this.y = 0
-        this.w = 50
-        this.h = 133
+  constructor(seme, valore, imgCarta) {
+    this.seme = seme;
+    this.valore = valore;
+    this.imgCarta = imgCarta;
+    this.x = 0;
+    this.y = 0;
+    this.w = 100;
+    this.h = 260;
+    this.dragging = false;
+  }
+  /**
+   *Function that chack if input x y point is on the card
+   *@param {number} xclick
+   *@param {number} yclick
+   */
+  isOnMe(xclick, yclick) {
+    if (xclick > this.x && xclick < this.x + this.w) {
+      if (yclick > this.y && yclick < this.y + this.h) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
     }
-
-    isOnMe(xclick, yclick){
-        if(xclick > this.x && xclick < (this.x+w)){
-            if(yclick > this.y && yclick < (this.y+h)){
-                return tree
-            }else{
-                return false
-            }
-        }else{
-            return false
-        }
-    }
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,99 +1,106 @@
-let c1;
-let percorsoFile = './Carte/';
-let estensione = '.jpg';
-let semi = ['B', 'C', 'D', 'S'];
-let nomiFileCarte = [];
 let mazzo = [];
-for (let x in semi){
-    for(let y = 1; y <= 13; y++){
-        nomiFileCarte.push(percorsoFile + y + semi[x] + estensione);
-    }
+let players = [];
+let offsetX = 0;
+let offsetY = 0;
+
+function preload() {
+  // seme , valore, immagine carta
+  let c1 = new Carta("B", 1, loadImage("./Carte/1B.jpg"));
+  let c2 = new Carta("D", 1, loadImage("./Carte/1D.jpg"));
+  let c3 = new Carta("C", 1, loadImage("./Carte/1C.jpg"));
+  c1.x = 100;
+  c1.y = 200;
+  c2.x = 200;
+  c2.y = 200;
+  c3.x = 300;
+  c3.y = 200;
+  mazzo.push(c1);
+  mazzo.push(c2);
+  mazzo.push(c3);
+  console.log(mazzo.length);
+  m1 = {
+    carte: mazzo
+  };
+  players.push(m1);
 }
-let m1
 
+function setup() {
+  canvas = createCanvas(700, 700);
+  bgcolor = color(200);
+}
 
-/*
-Funzione che carica le carte e popola il mazzo
-questa funzione fa parte della libreria p5js
-*/
-function preload(){
-    c1 = loadImage('./Carte/1B.jpg');
-    console.log(nomiFileCarte);
-    for (let x in semi){
-        for(let y = 1; y <= 13; y++){
-            mazzo.push(new Carta(semi[x], y, loadImage(percorsoFile + y + semi[x] + estensione)));
+function draw() {
+  background(bgcolor);
+  drawAllCardsFromConteiner(players);
+  updateCarte();
+}
+
+/**
+ *Metod that draw all 'cards' inside a given container arrey
+ * the arrey have to contain object with a property 'carte'
+ * the property carte have to be an array of 'Carta' object.
+ */
+function drawAllCardsFromConteiner(containers) {
+  //controllo che il conteiner non sia vuoto
+  if (containers.length < 1) {
+    return;
+  }
+  containers.forEach(container => {
+    if (container.carte.length > 0) {
+      container.carte.forEach(carta => {
+        image(carta.imgCarta, carta.x, carta.y, carta.w, carta.h);
+      });
+    }
+  });
+}
+
+function mousePressed() {
+  if (players.lenght < 1) {
+    return;
+  }
+  players.forEach(player => {
+    if (player.carte.length > 0) {
+      player.carte.forEach(carta => {
+        if (carta.isOnMe(mouseX, mouseY)) {
+          carta.dragging = true;
+          offsetX = mouseX - carta.x;
+          offsetY = mouseY - carta.y;
+          //carta.x = mouseX;
+          //carta.y = mouseY;
         }
+      });
     }
-    console.log(mazzo.length);
-    m1 = {
-        carte: mazzo
-    }
+  });
 }
 
-/*
-Funzione che inizializza lo stato iniziale
-questa funzione fa parte della libreria p5js
-*/
-function setup() { 
-    canvas = createCanvas(500, 500);
-    bgcolor = color(200);
-} 
-
-/*
-Funzione che continua a ridiregnare il camva,
-è in continuo loop, ogni volta che viene cambiato
-qualcosa il canvas si aggiorna.
-questa funzione fa parte della libreria p5js
-*/
-function draw() { 
-   background(bgcolor);
-   fill(255,0,175);
-   cc = ellipse(200, 200, 50, 50);
-   //image(c1, 20,20, 50,133);
-   //image(c1, 300,300, 25, 66);
-   //image(mazzo[30].imgCarta, 0, 0, 50, 133);
-   //drawAllCards()
-   drawAllCardsFromConteiner([m1])
+function mouseReleased() {
+  if (players.lenght < 1) {
+    return;
+  }
+  players.forEach(player => {
+    if (player.carte.length > 0) {
+      player.carte.forEach(carta => {
+        carta.dragging = false;
+        
+      });
+    }
+  });
 }
 
-/*
-Funzione di test che disegna tutte le carte dell'oggetto mazzo in ordine
-partendo dal punto 0,0 
-*/
-function drawAllCards(){
-    let x1 = 0
-    let y1 = 0
-    mazzo.forEach((carta) => {
-        image(carta.imgCarta, x1, y1, 25, 66)
-        carta.x = x1
-        carta.y = y1
-        x1 += 25
-        if(x1 >= 325){
-            x1 = 0
-            y1 += 70
+function updateCarte() {
+  if (players.lenght < 1) {
+    return;
+  }
+  players.forEach(player => {
+    if (player.carte.length > 0) {
+      player.carte.forEach(carta => {
+        if (carta.dragging) {
+          //offsetX = mouseX - carta.x;
+          //offsetY = mouseY - carta.y;
+          carta.x = mouseX - offsetX;
+          carta.y = mouseY - offsetY;
         }
-    })
-}
-
-
-/*
-Funzione che disegna tutte le carte contenute in vari "contenitori"
-questi contenitori possono essere:
-giocatori, il tavolo o altri
-questi contenitori vengono passati alla funzione attraverso un arrei
-ogni contenitore dove possedere un arrei di nome carte che contiene
-le carte in suo possesso
-*/
-function drawAllCardsFromConteiner(containers){
-    //controllo che il conteiner non sia vuoto
-    if(containers.length < 1){
-        return
+      });
     }
-    containers.forEach((container) =>{
-        if(container.carte.length > 0){
-            container.carte.forEach((carta) => {
-                image(carta.imgCarta, carta.x, carta.y, carta.w, carta.h)
-            })
-        }
-    })
+  });
 }
